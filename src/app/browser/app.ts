a5c6c398ab7f52cf2442f2f8e093f14f56ac69bf
darkness <tw1nkle001@yandex.ru>
2026-04-28 23:49:45 +0700

    add route ui

--- src/app/browser/app.ts
+++ src/app/browser/app.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PopupSubscribeComponent, OverheaderComponent } from '../../components';
+import {
+  PopupSubscribeComponent,
+  OverheaderComponent,
+  HeaderComponent,
+  FooterComponent,
+} from '../../components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PopupSubscribeComponent, OverheaderComponent],
+  imports: [
+    RouterOutlet,
+    PopupSubscribeComponent,
+    OverheaderComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.less',
 })

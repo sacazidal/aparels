bf477c82c9024c72947f16fa8cf8f041e4646f6c
darkness <tw1nkle001@yandex.ru>
2026-04-28 23:49:45 +0700

    add route ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UI_ROUTES } from '../core/constants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: UI_ROUTES.DEFAULT,
+    redirectTo: UI_ROUTES.HOME,
+    pathMatch: 'full',
+  },
+  {
+    path: UI_ROUTES.HOME,
+    loadComponent: () => import('../pages').then((m) => m.HomePage),
+  },
+];
--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,2 +1,3 @@
 export { API_ROUTE } from './api-route';
 export { STORAGE_KEY } from './storage-key';
+export { UI_ROUTES } from './ui-routes';

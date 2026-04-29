e373b208ca196387a4d1ab193f3871ab7d005f84
darkness <tw1nkle001@yandex.ru>
2026-04-29 17:25:54 +0700

    add contacy-us form in form-factory service

--- src/core/services/form-factory/form-factory.spec.ts
+++ src/core/services/form-factory/form-factory.spec.ts
@@ -33,4 +33,34 @@ describe('FormFactoryService', () => {
       expect(form.valid).toBeTruthy();
     });
   });
+
+  describe('contactUs', () => {
+    it('should mark form as invalid when empty or filled with incorrect data', () => {
+      const form = service.contactUs();
+      expect(form.valid).toBeFalsy();
+
+      form.patchValue({
+        email: 'test',
+        name: 'Test',
+        phone: '89999999999',
+        message: 'test-text',
+      });
+
+      expect(form.valid).toBeFalsy();
+    });
+
+    it('should mark form as valid when all fileds are filled', () => {
+      const form = service.contactUs();
+      expect(form.valid).toBeFalsy();
+
+      form.patchValue({
+        email: 'test@test.ru',
+        name: 'Test',
+        phone: '89999999999',
+        message: 'test-text',
+      });
+
+      expect(form.valid).toBeTruthy();
+    });
+  });
 });
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 export type { BuilderType } from './form/builder-type';
 export type { SubscribeNews } from './form/subscribe';
+export type { Contact } from './form/contact';
 export type * from './nav-item';
 export type { Earrings } from '../models/earring';
